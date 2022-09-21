aecc0ee2f3f2b236735da7410191afa45b0eb483
MedAnypli <mebensalem@anypli.com>
2022-09-21 15:56:41 +0100

    update the package.Swift dependencies

--- Package.swift
+++ Package.swift
@@ -14,8 +14,10 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-//        .package(url: "https://github.com/facebook/facebook-ios-sdk", exact: "6.9.0")
+//         .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/facebook/facebook-ios-sdk", exact: "14.0.0"),
+        .package(url: "https://github.com/criteo/ios-publisher-sdk", exact: "4.5.0"),
+        .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads", exact: "9.9.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,6 +31,6 @@ let package = Package(
         .binaryTarget(name: "BlueStackFacebookAdapter", path: "BlueStackFacebookAdapter.xcframework"),
         .binaryTarget(name: "BlueStackLocationAdapter", path: "BlueStackLocationAdapter.xcframework"),
         .binaryTarget(name: "BlueStackOguryAdapter", path: "BlueStackOguryAdapter.xcframework"),
-        .binaryTarget(name: "BlueStackSASAdapter", path: "BlueStackSDK/BlueStackSASAdapter.xcframework"),
+        .binaryTarget(name: "BlueStackSASAdapter", path: "BlueStackSASAdapter.xcframework"),
     ]
 )
