a9ecc8175140d3ee0f665db50e755b098fcf95dc
MedAnypli <mebensalem@anypli.com>
2023-02-15 13:47:02 +0100

    test

--- Package.swift
+++ Package.swift
@@ -5,27 +5,20 @@ import PackageDescription
 
 let package = Package(
     name: "BlueStackSPMTest",
+    
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BlueStackSPMTest",
-            targets: ["BlueStackSDK",
-                      "OMSDK_Madvertise",
-                      "BlueStackAdColonyAdapter",
-                      "BluestackAmazonPublisherServicesAdapter",
-                      "BluestackCriteoAdapter",
-                      "BlueStackDFPAdapter",
-                      "BlueStackFacebookAdapter",
-                      "BlueStackLocationAdapter",
-                      "BlueStackOguryAdapter",
-                      "BlueStackSASAdapter"]),
+            targets: ["BlueStackSDK"]),
     ],
     dependencies:  [
-        .package(url: "https://mdtb-sdk-packages.s3-us-west-2.amazonaws.com/iOS_APS_SDK/APS_iOS_SDK-4.5.6.zip", .exact("4.5.5")),
+//        .package(url: "https://mdtb-sdk-packages.s3-us-west-2.amazonaws.com/iOS_APS_SDK/APS_iOS_SDK-4.5.6.zip", .exact("4.5.5")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .target(name: "BlueStackSDK", dependencies: ["BlueStackSDK","OMSDK_Madvertise", "AmazonPublisherServicesSDK", "AdColony"]),
         .binaryTarget(name: "BlueStackSDK", path: "BlueStackSDK.xcframework"),
         .binaryTarget(name: "OMSDK_Madvertise", path: "OMSDK_Madvertise.xcframework"),
         .binaryTarget(name: "BlueStackAdColonyAdapter", path: "BlueStackAdColonyAdapter.xcframework"),
