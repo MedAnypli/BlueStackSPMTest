af68a10333ff1c763acf95dc2fa3f2cf4a5c6766
MedAnypli <mebensalem@anypli.com>
2023-02-16 09:46:00 +0100

    add package https://github.com/google/promises

--- Package.swift
+++ Package.swift
@@ -16,6 +16,8 @@ let package = Package(
 //        //        .package(url: "https://mdtb-sdk-packages.s3-us-west-2.amazonaws.com/iOS_APS_SDK/APS_iOS_SDK-4.5.6.zip", .exact("4.5.5")),
 //        .package(url: "https://github.com/BlueStack-SDK/Core", from: "1.0.0"),
 //        .package(url: "https://github.com/smartadserver/Smart-Display-SDK-iOS.git", .upToNextMajor(from: "7.19.1"))
+        //PromisesObjC
+        .package(url: "https://github.com/google/promises", exact: "2.1.1")
         
     ],
     targets: [
