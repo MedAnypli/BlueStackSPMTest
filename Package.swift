cb76dea08a6dc4934844323fb7d1770019695f42
MedAnypli <mebensalem@anypli.com>
2023-02-17 12:08:16 +0100

    fix the path of PromisesObjC

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
             url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git",
             "1.1.0"..<"3.0.0"
         ),
-        .package(path: "Dependencies/PromisesObjC")
+        .package(path: "../Dependencies/PromisesObjC")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
