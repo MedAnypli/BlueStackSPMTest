ee9eab41c78e0800c0cc671a71c1ee224ba87195
MedAnypli <mebensalem@anypli.com>
2023-02-17 17:15:34 +0100

    ok

--- Package.swift
+++ Package.swift
@@ -66,8 +66,7 @@ let package = Package(
                         condition: .when(platforms: [.iOS])
                     ),
                     .target(
-                        name: "FBLPromises",
-                        path: "Dependencies/FBLPromises"
+                        name: "FBLPromises"
                     ),
                     //                       .target(name: "GoogleAppMeasurement", condition: .when(platforms: [.iOS])),
                     //                       .target(name: "GoogleAppMeasurementIdentitySupport", condition: .when(platforms: [.iOS])),
@@ -106,9 +105,9 @@ let package = Package(
                     .linkedLibrary("sqlite3")
                 ]
                ),
-//        .target(name: "FBLPromises",
-//                path: "Dependencies/FBLPromises"
-//               ),
+        .target(name: "FBLPromises",
+                path: "Dependencies/FBLPromises"
+               ),
         .binaryTarget(name: "BlueStackSDK", path: "BlueStackSDK.xcframework"),
         .binaryTarget(name: "OMSDK_Madvertise", path: "OMSDK_Madvertise.xcframework"),
         //smart
