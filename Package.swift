7dc32d6fd183ce62cf935e5f17417b1135406efe
MedAnypli <mebensalem@anypli.com>
2023-02-21 17:32:41 +0100

    cvvv

--- Package.swift
+++ Package.swift
@@ -28,6 +28,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "BlueStackSDKWrapper",
                 dependencies: [
+                    "FBLPromises",
                     .target(name: "BlueStackSDK", condition: .when(platforms: [.iOS])),
                     .target(name: "OMSDK_Madvertise", condition: .when(platforms: [.iOS])),
                     //smart
