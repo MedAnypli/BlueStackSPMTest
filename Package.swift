c2f0a266bf60a477b8b6e3a368b295fbbace3c56
MedAnypli <mebensalem@anypli.com>
2023-02-21 11:10:22 +0100

    ccc

--- Package.swift
+++ Package.swift
@@ -89,7 +89,7 @@ let package = Package(
                 ],
                 path: "BlueStackDFPAdapterWrapper",
                 linkerSettings: [
-                    .linkedLibrary("FBLPromises")
+                    .linkedLibrary("Promises")
                 ]
                ),
         .binaryTarget(name: "BlueStackSDK", path: "BlueStackSDK.xcframework"),
