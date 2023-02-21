83163a6884a3a33731952c458fd37ef9044dc932
MedAnypli <mebensalem@anypli.com>
2023-02-21 16:45:20 +0100

    cc

--- Package.swift
+++ Package.swift
@@ -14,10 +14,10 @@ let package = Package(
         //            targets: ["BlueStackSDK" , "OMSDK_Madvertise", "BlueStackSASAdapter", "SASDisplayKit", "SCSCoreKit", "BluestackAmazonPublisherServicesAdapter", "DTBiOSSDK", "BlueStackLocationAdapter" ,"MAdvertiseLocation","BlueStackDFPAdapter","GoogleMobileAds"]),
     ],
     dependencies:  [
-//        .package(
-//            name: "GoogleMobileAds",
-//            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "9.13.0"),
-//        .package(url: "https://github.com/google/promises.git", from: "2.2.0")
+        //        .package(
+        //            name: "GoogleMobileAds",
+        //            url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", from: "9.13.0"),
+        //        .package(url: "https://github.com/google/promises.git", from: "2.2.0")
         .package(
             name: "FBLPromises",
             url: "https://github.com/google/promises.git",
@@ -44,29 +44,16 @@ let package = Package(
                     //location
                     .target(name: "BlueStackLocationAdapter", condition: .when(platforms: [.iOS])),
                     .target(name: "MAdvertiseLocation", condition: .when(platforms: [.iOS])),
-//                    .target(name: "BlueStackImproveAdapter", condition: .when(platforms: [.iOS])),
-                     //comment for now because we need FBLPromises
+                    //                    .target(name: "BlueStackImproveAdapter", condition: .when(platforms: [.iOS])),
+                    //comment for now because we need FBLPromises
                     //dfp
-                    .target(
-                        name: "BlueStackDFPAdapterWrapper",
-                        condition: .when(platforms: [.iOS])
-                    ),
-                    //                       .target(name: "GoogleAppMeasurement", condition: .when(platforms: [.iOS])),
-                    //                       .target(name: "GoogleAppMeasurementIdentitySupport", condition: .when(platforms: [.iOS])),
-//                    .target(name: "GoogleMobileAds", condition: .when(platforms: [.iOS])),
-//                    .target(name: "GoogleUtilities", condition: .when(platforms: [.iOS])),
-//                    .target(name: "nanopb", condition: .when(platforms: [.iOS])),
-//                    .target(name: "UserMessagingPlatform", condition: .when(platforms: [.iOS])),
-//                    .target(name: "PromisesObjC",
-//                            condition: .when(platforms: [.iOS])),
+                        .target(
+                            name: "BlueStackDFPAdapterWrapper",
+                            condition: .when(platforms: [.iOS])
+                        ),
                     //ogury
-                        .target(name: "BlueStackOguryAdapter", condition: .when(platforms: [.iOS])),
+                    .target(name: "BlueStackOguryAdapter", condition: .when(platforms: [.iOS])),
                     .target(name: "OMSDK_Ogury", condition: .when(platforms: [.iOS])),
-//                    .product(name: "swift-package-manager-google-mobile-ads",
-//                             package: "swift-package-manager-google-mobile-ads"),
-                    //                    .product(name: "GoogleUserMessagingPlatform", package: "GoogleUserMessagingPlatform"),
-                    //                    .product(name: "Promises",
-                    //                             package: "Promises"),
                 ],
                 path: "BlueStackSDKWrapper",
                 linkerSettings: [
@@ -92,43 +79,35 @@ let package = Package(
                ),
         .target(name: "BlueStackDFPAdapterWrapper",
                 dependencies: [
-//                    "FBLPromises",
+                    //                    "FBLPromises",
                     "BlueStackDFPAdapter" ,
                     "GoogleMobileAds",
-//                    "PromisesObjC",
-//                    .product(name: "GoogleMobileAds", package: "GoogleMobileAds"),
-//                    .target(name: "BlueStackDFPAdapter", condition: .when(platforms: [.iOS])),
+                    //                    "PromisesObjC",
+                    //                    .product(name: "GoogleMobileAds", package: "GoogleMobileAds"),
+                    //                    .target(name: "BlueStackDFPAdapter", condition: .when(platforms: [.iOS])),
                 ],
-                path: "BlueStackDFPAdapterWrapper",
-//                linkerSettings: [
-//                    .linkedLibrary("FBLPromises")
-//                ]
+                path: "BlueStackDFPAdapterWrapper"
                ),
         .binaryTarget(name: "BlueStackSDK", path: "BlueStackSDK.xcframework"),
         .binaryTarget(name: "OMSDK_Madvertise", path: "OMSDK_Madvertise.xcframework"),
-//        .target(name: "FBLPromises",
-//               path: "Dependencies/FBLPromises"),
         //smart
         .binaryTarget(name: "BlueStackSASAdapter", path: "BlueStackSASAdapter.xcframework"),
+        .binaryTarget(name: "SASDisplayKit", path: "Dependencies/SASDisplayKit.xcframework"),
+        .binaryTarget(name: "SCSCoreKit", path: "Dependencies/SCSCoreKit.xcframework"),
+        //adcolony
         .binaryTarget(name: "BlueStackAdColonyAdapter", path: "BlueStackAdColonyAdapter.xcframework"),
+        .binaryTarget(name: "AdColony", path: "Dependencies/AdColony.xcframework"),
         .binaryTarget(name: "BluestackCriteoAdapter", path: "BluestackCriteoAdapter.xcframework"),
         //dfp
         .binaryTarget(name: "BlueStackDFPAdapter", path: "BlueStackDFPAdapter.xcframework"),
-        //        .binaryTarget(name: "GoogleAppMeasurement", path: "Dependencies/GoogleAppMeasurement.xcframework"),
-        //        .binaryTarget(name: "GoogleAppMeasurementIdentitySupport", path: "Dependencies/GoogleAppMeasurementIdentitySupport.xcframework"),
-            .binaryTarget(name: "GoogleMobileAds", path: "Dependencies/GoogleMobileAds.xcframework"),
-//                .binaryTarget(name: "GoogleUtilities", path: "Dependencies/GoogleUtilities.xcframework"),
-//                .binaryTarget(name: "nanopb", path: "Dependencies/nanopb.xcframework"),
-//            .binaryTarget(name: "PromisesObjC", path: "Dependencies/PromisesObjC.xcframework"),
-//                .binaryTarget(name: "UserMessagingPlatform", path: "Dependencies/UserMessagingPlatform.xcframework"),
-            .binaryTarget(name: "BlueStackImproveAdapter", path: "BlueStackImproveAdapter.xcframework"),
+        .binaryTarget(name: "GoogleMobileAds", path: "Dependencies/GoogleMobileAds.xcframework"),
+        .binaryTarget(name: "BlueStackImproveAdapter", path: "BlueStackImproveAdapter.xcframework"),
         //fb
-            .binaryTarget(name: "BlueStackFacebookAdapter", path: "BlueStackFacebookAdapter.xcframework"),
+        .binaryTarget(name: "BlueStackFacebookAdapter", path: "BlueStackFacebookAdapter.xcframework"),
         .binaryTarget(name: "BlueStackLocationAdapter", path: "BlueStackLocationAdapter.xcframework"),
         .binaryTarget(name: "BlueStackOguryAdapter", path: "BlueStackOguryAdapter.xcframework"),
         .binaryTarget(name: "BluestackAmazonPublisherServicesAdapter", path: "BluestackAmazonPublisherServicesAdapter.xcframework"),
         //the dependencies
-        .binaryTarget(name: "AdColony", path: "Dependencies/AdColony.xcframework"),
         .binaryTarget(name: "DTBiOSSDK", path: "Dependencies/DTBiOSSDK.xcframework"),
         .binaryTarget(name: "FBAudienceNetwork", path: "Dependencies/FBAudienceNetwork.xcframework"),
         .binaryTarget(name: "ImproveDigital", path: "Dependencies/ImproveDigital.xcframework"),
@@ -136,7 +115,5 @@ let package = Package(
         .binaryTarget(name: "OguryAds", path: "Dependencies/OguryAds.xcframework"),
         .binaryTarget(name: "OguryChoiceManager", path: "Dependencies/OguryChoiceManager.xcframework"),
         .binaryTarget(name: "OMSDK_Ogury", path: "Dependencies/OMSDK_Ogury.xcframework"),
-        .binaryTarget(name: "SASDisplayKit", path: "Dependencies/SASDisplayKit.xcframework"),
-        .binaryTarget(name: "SCSCoreKit", path: "Dependencies/SCSCoreKit.xcframework"),
     ]
 )
